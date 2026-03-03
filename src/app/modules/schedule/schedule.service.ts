b1034a6fb3914d1c120e35edca50ae1094cb7201
Arman3747 <dailyarman7@gmail.com>
2026-03-04 04:21:30 +1100

    schedule added

---
+++ src/app/modules/schedule/schedule.service.ts
@@ -0,0 +1,99 @@
+import { compareAsc, format, addHours, addMinutes } from "date-fns";
+import { prisma } from "../../../../lib/prisma";
+const insertIntoDB = async (payload: any) => {
+  const { startTime, endTime, startDate, endDate } = payload;
+
+  const intervalTime = 30;
+
+  const schedules = [];
+
+  const currentDate = new Date(startDate);
+  const lastDate = new Date(endDate);
+
+  while (currentDate <= lastDate) {
+    const startDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(startTime.split(":")[0]),
+        ),
+        Number(startTime.split(":")[1]),
+      ),
+    );
+
+    const endDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(endTime.split(":")[0]),
+        ),
+        Number(endTime.split(":")[1]),
+      ),
+    );
+
+    while (startDateTime < endDateTime) {
+      const slotStartDateTime = startDateTime; // 10:00
+      const slotEndDateTime = addMinutes(startDateTime, intervalTime); // 10:30
+
+      const scheduleData = {
+        startDateTime: slotStartDateTime,
+        endDateTime: slotEndDateTime,
+      };
+
+      const existingSchedule = await prisma.schedule.findFirst({
+        where: scheduleData,
+      });
+
+      if (!existingSchedule) {
+        const result = await prisma.schedule.create({
+          data: scheduleData,
+        });
+        schedules.push(result);
+      }
+
+      slotStartDateTime.setMinutes(
+        slotStartDateTime.getMinutes() + intervalTime,
+      );
+    }
+
+    /**BETTER while code
+     * 
+     * while (startDateTime < endDateTime) {
+  const slotStartDateTime = new Date(startDateTime); // clone
+  const slotEndDateTime = addMinutes(
+    new Date(startDateTime),
+    intervalTime
+  );
+
+  const scheduleData = {
+    startDateTime: slotStartDateTime,
+    endDateTime: slotEndDateTime,
+  };
+
+  const existingSchedule = await prisma.schedule.findFirst({
+    where: scheduleData,
+  });
+
+  if (!existingSchedule) {
+    const result = await prisma.schedule.create({
+      data: scheduleData,
+    });
+    schedules.push(result);
+  }
+
+  // update original startDateTime safely
+  startDateTime.setMinutes(
+    startDateTime.getMinutes() + intervalTime
+  );
+}
+    */
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return schedules;
+};
+
+export const ScheduleService = {
+  insertIntoDB,
+};
