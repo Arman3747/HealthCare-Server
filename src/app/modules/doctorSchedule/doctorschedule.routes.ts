54decb6b5ec8de84e95d05e82a67497295d52f78
Arman3747 <dailyarman7@gmail.com>
2026-03-09 17:29:10 +1100

    specialities

--- src/app/modules/doctorSchedule/doctorschedule.routes.ts
+++ src/app/modules/doctorSchedule/doctorschedule.routes.ts
@@ -2,9 +2,18 @@ import express from "express";
 import { doctorScheduleController } from "./doctorSchedule.controller";
 import auth from "../../middlewares/auth";
 import { UserRole } from "../../../../generated/prisma/enums";
+import validateRequest from "../../middlewares/validateRequest";
+import { doctorScheduleValidation } from "./doctorSchedule.validation";
 
 const router = express.Router();
 
-router.post("/", auth(UserRole.DOCTOR), doctorScheduleController.insertIntoDB);
+router.post(
+  "/",
+  auth(UserRole.DOCTOR),
+  validateRequest(
+    doctorScheduleValidation.createDoctorScheduleValidationSchema,
+  ),
+  doctorScheduleController.insertIntoDB,
+);
 
 export const doctorScheduleRoutes = router;
